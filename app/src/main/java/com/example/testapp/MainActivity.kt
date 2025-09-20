c9fc9c1791a80ba96bae63ab866a80578b4832a2
ryanesok <zakkidash@gamil.com>
2025-09-20 14:38:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.testapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    var angkaBulat: Int = 0
+
+    fun hitung(){
+        angkaBulat++
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.text_view)
+        val btnHitung: Button = findViewById(R.id.btn_hitung)
+
+        btnHitung.setOnClickListener({
+            hitung()
+            textView.text = angkaBulat.toString()
+        })
+    }
+}
